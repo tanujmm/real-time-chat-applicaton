a028219e3b77ee84b4f58347d44fee47d779bfd8
tanujmm <mtanuj121@gmail.com>
2025-07-03 12:56:23 +0530

    change

--- server/server.js
+++ server/server.js
@@ -11,26 +11,26 @@ import { Server } from "socket.io";
 const app = express();
 
 const server = http.createServer(app);
-export const io=new Server(server,{
-  cors:{origin:"*"}
-})
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
 
-export const userSocketMap={}
+export const userSocketMap = {};
 
-io.on("connection",(socket)=>{
-  const userId=socket.handshake.query.userId
-  console.log("User Connected",userId);
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
 
-  if(userId) userSocketMap[userId]=socket.id;
-  io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  if (userId) userSocketMap[userId] = socket.id;
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-  socket.on("disconnect",()=>{
-    console.log("User Disconnected",userId);
-    delete userSocketMap[userId]
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
-  })
-})
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
@@ -38,6 +38,10 @@ app.use("/api/status", (req, res) => {
   res.send("Server is live");
 });
 app.use("/api/auth", userRouter);
+app.get("/", (req, res) => {
+  res.send("API is runningg...");
+});
+
 app.use("/api/messages", messageRouter);
 await connectDB();
 const PORT = process.env.PORT || 5000;
